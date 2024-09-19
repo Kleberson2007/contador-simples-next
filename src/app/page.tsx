28bf6ac099360c8af3f654604da3f535449ad518
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-09-19 14:59:19 -0300

    ]

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { useState } from "react";
+import "./globals.css";
+
+export default function Home() {
+  const [numero, setNumero] = useState(0)
+  
+  function aumentar1() {
+    setNumero(numero + 1)
+  }
+
+  function diminuir1() {
+    setNumero(numero - 1)
+  }
+
+  return (
+    <div>
+      <h1>contador</h1>
+      <p>{numero}</p>
+      <div id="botoes">
+        <button onClick={aumentar1}>+1</button>
+        <button onClick={diminuir1}>-1</button>
+      </div>
+    </div>
+  );
+}
